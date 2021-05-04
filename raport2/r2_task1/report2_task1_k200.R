da35c831eff1b3e204d7e9f50722dc26296ccc0b
BartosZimonczyk <bartosz.zimonczyk@gmail.com>
2021-05-04 17:59:40 +0200

    Fixed bugs.

--- raport2/r2_task1/report2_task1_k200.R
+++ raport2/r2_task1/report2_task1_k200.R
@@ -10,8 +10,6 @@ library(xtable)
 # 2. calculate the bias, the variance and MSE of above optimal estimator 
 # 3. Find critical value and calculate the power of the statistical
 #    test based on the ridge regression and controlling FWER at level 0.1
-#    REMINDER: controlling FWER at given level is equavilent to controlling
-#              probability of type I error at the same level
 
 
 final_table <- matrix(nrow=1, ncol=9)
@@ -21,47 +19,54 @@ optimal_lambdas <- c()
 ptm <- proc.time()
 j <- 1
 
+cat('Computing orthonormal matrix...\n')
+X <- matrix(rnorm(950*1000), nrow=1000, ncol=950)
+X <- orthonormalization(X, FALSE, TRUE)
 b <- c(rep(3.5, k), rep(0, 950-k))
-b_hat_ridge <- matrix(nrow=950, ncol=rp1)
-b_hat_ols <- matrix(nrow=950, ncol=rp1)
+b_hat_ridge <- matrix(nrow=rp1, ncol=950)
+b_hat_ols <- matrix(nrow=rp1, ncol=950)
+bias_ridge <- c()
+bias_ols <- c()
+mse_ridge_iter <- c()
+mse_ols_iter <- c()
 power_ridge_vector <- c()
 power_ols_vector <- c()
 for(i in 1:rp1){
-  X <- matrix(rnorm(1000 * 950, 0, sqrt(1/1000)), nrow=1000)
   eps <- rnorm(1000)
   Y <- X %*% b + eps
-
+  
   ridge_obj <- cv.glmnet(X, Y, standarize=F, intercept=F, alpha=0)
   ols_obj <- lm(Y ~ X-1)
-
+  
   optimal_lambdas[i] <- ridge_obj$lambda.min
-
-
-  b_hat_ridge[,i] <- coef(ridge_obj, s='lambda.min')[2:951]
-  b_hat_ols[,i] <- summary(ols_obj)$coefficients[,1]
-
+  
+  b_hat_ridge[i,] <- coef(ridge_obj, s='lambda.min')[2:951]
+  b_hat_ols[i,] <- summary(ols_obj)$coefficients[,1]
+  
+  bias_ridge[i] <- sum((b_hat_ridge[i,] - b)^2)
+  bias_ols[i] <- sum((b_hat_ols[i,] - b)^2)
+  
   # TODO: add power computation
   gamma_ridge <- optimal_lambdas[i]
-  TD_ridge <- sum(abs(b_hat_ridge[1:k,i]) > 1/(1+gamma_ridge) * qnorm(1-0.1/2))
+  TD_ridge <- sum(abs(b_hat_ridge[i,1:k]) > 1/(1+gamma_ridge) * qnorm(1-0.1/2/950))
   power_ridge_vector[i] <- TD_ridge/k
-
+  
   gamma_ols <- 0
-  TD_ols <- sum(abs(b_hat_ols[1:k,i]) > 1/(1+gamma_ols) * qnorm(1-0.1/2))
+  TD_ols <- sum(abs(b_hat_ols[i,1:k]) > 1/(1+gamma_ols) * qnorm(1-0.1/2/950))
   power_ols_vector[i] <- TD_ols/k
-
+  
+  mse_ridge_iter[i] <- sum((Y - X%*%b_hat_ridge[i,])^2)
+  mse_ols_iter[i] <- sum((Y - X%*%b_hat_ols[i,])^2)
+  
   cat('Iteration: ', i, 'for k = ', k, '\n')
   cat('Time elapsed: ', (proc.time()-ptm)[3], '\n')
 }
-mean_b_hat_ridge <- apply(b_hat_ridge, 1, mean)
-mean_b_hat_ols <- apply(b_hat_ols, 1, mean)
-var_b_hat_ridge <- apply(b_hat_ridge, 1, var)
-var_b_hat_ols <- apply(b_hat_ols, 1, var)
-bias_ridge <- sum((mean_b_hat_ridge - b)^2)
-bias_ols <- sum((mean_b_hat_ols - b)^2)
-var_ridge <- sum(var_b_hat_ridge)
-var_ols <- sum(var_b_hat_ols)
-mse_ridge <- var_ridge + bias_ridge
-mse_ols <- var_ols + bias_ols
+var_ridge <- sum(apply(b_hat_ridge, 2, var))
+var_ols <- sum(apply(b_hat_ols, 2, var))
+bias_ridge <- mean(bias_ridge)
+bias_ols <- mean(bias_ols)
+mse_ridge <- mean(mse_ridge_iter)
+mse_ols <- mean(mse_ols_iter)
 power_ridge <- mean(power_ridge_vector)
 power_ols <- mean(power_ols_vector)
 
@@ -77,11 +82,6 @@ final_table[j,] <- c(
   mean(optimal_lambdas)
 )
 
-
-final_table <- as.data.frame(
-  final_table,
-  row.names = c('k=20', 'k=100', 'k=200')
-)
 colnames(final_table) <- c(
   'Bias_OLS',
   'Bias_ridge',
@@ -99,28 +99,6 @@ write.csv(final_table, file=file_name)
 
 #############################################################
 # task 2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
